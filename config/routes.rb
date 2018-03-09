aca62c99e3db3a009cd987580da71a010742caa6
Andrea <andrea.africa93@gmail.com>
2018-03-09 16:06:04 -0500

    routes got rid of amafrica#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'amafrica#index'
 
   resources :amafrica, only: [:index, :show]
 
